96bf5bbe1cb1670d95122eb87d447f96362aaf11
YasTaqaa <ilyasawt@gmail.com>
2025-06-19 19:57:39 +0700

    grap

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -3,16 +3,17 @@ import { startServerAndCreateNextHandler } from '@as-integrations/next'
 import { ApolloServer } from '@apollo/server'
 import { typeDefs, resolvers } from '@/graphql/schema'
 import { connectDB } from '@/lib/db/connectDB'
+import { NextRequest } from 'next/server'
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
 
-const handler = startServerAndCreateNextHandler(server, {
-  context: async () => {
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async (req: NextRequest) => {
     await connectDB()
-    return {}
+    return { req }
   },
 })
 
